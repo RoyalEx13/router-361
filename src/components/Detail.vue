1e7cff9be749cc740b0010c23fb57ec1a9602d59
RoyalEx13 <nonty009@hotmail.com>
2022-02-14 19:54:32 +0700

    first commit

---
+++ src/components/Detail.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container grid-list-xs>
+    <h1>Detail page</h1>
+    <v-card
+        class="mx-auto "
+        max-width="7000"
+        v-for="(item, i) in dt"
+        :key="i"
+      >
+        <v-card-text v-if=" i+1 == productid" >
+          <h1 class="text-decoration-none text-right text-md-left my-5 text-black">{{ item.title }}</h1>
+          <v-img :src="item.image" ></v-img>
+          <div class="display-1 my-5 ">{{ item.description }}</div>
+          <v-btn color="black" text> Category: {{ item.category }} </v-btn>
+            <v-btn color="green darken-2" text outlined large>
+              Price: {{ item.price }}
+            </v-btn>
+            <v-btn color="orange darken-2" text outlined large>
+              rating⭐️:{{ item.rating.rate }} ({{
+                item.rating.count
+              }})</v-btn
+            >
+        </v-card-text>
+
+      </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["dt"],
+  data() {
+    return {
+
+      productid: this.$route.params.id,
+    };
+  },
+};
+</script>
+
+<style></style>
